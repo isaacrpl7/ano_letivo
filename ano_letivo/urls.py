73583458063e522ee43553caef80627f80520935
Isaac Reinaldo <isaacrpl7@hotmail.com>
2021-02-01 16:32:23 -0300

    URLS

--- ano_letivo/urls.py
+++ ano_letivo/urls.py
@@ -20,6 +20,7 @@ from dashboard.views import signup
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('', include('dashboard.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', signup.signup_view, name="signup" )
 ]
